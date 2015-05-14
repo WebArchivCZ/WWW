74ff897099c1ba180a0f2ecef822754374621428
Jaroslav Kvasnica <jarda@wapublic2.webarchiv.cz>
2015-05-14 14:07:41 +0200

    oprava odkazu na smlouvu (latte/cooperation) a umisteni smlouvy do adresare download

--- temp/cache/_Nette.FileTemplate/_Default.cooperation.latte-7d8f0c7c072824d1f795e37f502e3399.php
+++ temp/cache/_Nette.FileTemplate/_Default.cooperation.latte-7d8f0c7c072824d1f795e37f502e3399.php
@@ -1,31 +1,31 @@
-<?php //netteCache[01]000382a:2:{s:4:"time";s:21:"0.66025300 1429901964";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:68:"/srv/www/htdocs/webarchiv.cz/app/templates/Default/cooperation.latte";i:2;i:1429718033;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:22:"released on 2015-01-06";}}}?><?php
+<?php //netteCache[01]000382a:2:{s:4:"time";s:21:"0.12050100 1431605180";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:68:"/srv/www/htdocs/webarchiv.cz/app/templates/Default/cooperation.latte";i:2;i:1431605159;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:22:"released on 2015-01-06";}}}?><?php
 
 // source file: /srv/www/htdocs/webarchiv.cz/app/templates/Default/cooperation.latte
 
 ?><?php
 // prolog Nette\Latte\Macros\CoreMacros
-list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, 'hfdkds6cd9')
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, '777tvubma9')
 ;
 // prolog Nette\Latte\Macros\UIMacros
 //
 // block title
 //
-if (!function_exists($_l->blocks['title'][] = '_lbe841cd585f_title')) { function _lbe841cd585f_title($_l, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+if (!function_exists($_l->blocks['title'][] = '_lb2c3b4f6cbd_title')) { function _lb2c3b4f6cbd_title($_l, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
 ?>Pomozte nám uchovat český web | <?php Nette\Latte\Macros\UIMacros::callBlockParent($_l, 'title', get_defined_vars()) ;
 }}
 
 //
 // block menuItemActive4
 //
-if (!function_exists($_l->blocks['menuItemActive4'][] = '_lbe8d8118bd9_menuItemActive4')) { function _lbe8d8118bd9_menuItemActive4($_l, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+if (!function_exists($_l->blocks['menuItemActive4'][] = '_lbcd7bc89cdf_menuItemActive4')) { function _lbcd7bc89cdf_menuItemActive4($_l, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
 ?> class="active"<?php
 }}
 
 //
 // block content
 //
-if (!function_exists($_l->blocks['content'][] = '_lbd2cce55677_content')) { function _lbd2cce55677_content($_l, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
-;Nette\Latte\Macros\CoreMacros::includeTemplate("../commonParts/add.latte", $template->getParameters(), $_l->templates['hfdkds6cd9'])->render() ?>
+if (!function_exists($_l->blocks['content'][] = '_lbe3d181b48b_content')) { function _lbe3d181b48b_content($_l, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+;Nette\Latte\Macros\CoreMacros::includeTemplate("../commonParts/add.latte", $template->getParameters(), $_l->templates['777tvubma9'])->render() ?>
 
 	<section>
 		<div class="container">
@@ -41,7 +41,7 @@ if (!function_exists($_l->blocks['content'][] = '_lbd2cce55677_content')) { func
 				<li>My vás kontaktujeme a pokusíme se s vámi uzavřít smlouvu, která nám umožní zpřístupňovat archiv Vašich webových stránek online přímo přes internet.</li>
 				<li>Vystavili jste obsah Vašich stránek pod licencí <a href="<?php echo Nette\Templating\Helpers::escapeHtml($_presenter->link("creativeCommons"), ENT_COMPAT) ?>
 ">Creative Commons</a> (CC)? <a href="<?php echo Nette\Templating\Helpers::escapeHtml($_presenter->link("add"), ENT_COMPAT) ?>">Ozvěte se nám!</a></li>
-				<li>Na náhled smlouvy o poskytování elektronických online zdrojů se můžete podívat <a href="http://www.webarchiv.cz/file_download/12/smlouvaWebarchiv.doc">zde</a>.</li>
+				<li>Na náhled smlouvy o poskytování elektronických online zdrojů se můžete podívat <a href="http://webarchiv.cz/download/smlouvaWebarchiv.doc">zde</a>.</li>
 			</ul>
 
 			<h2>Co získáte spoluprácí s námi?</h2>
