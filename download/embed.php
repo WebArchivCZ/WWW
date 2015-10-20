2886dc7f4a8d4d23557584c1feb25a236e86ad7c
Rudolf <westfood@gmail.com>
2015-10-20 13:38:28 +0200

    oprava zdrojoveho kodu pro banner

--- download/embed.php
+++ download/embed.php
@@ -13,7 +13,7 @@
 
 <body>
 <p style="font: 1em Courier">
-&lt;a href="http://www.webarchiv.cz/cs/certifikovano" onclick="return !window.open(this, 'kod', 'toolbar=no, menubar=no, directories=no, resizable=yes, status=no, width=600, height=210, top=200, left=50')" &lt;img alt="Stránky jsou archivovány Národní knihovnou ČR" style="border:none" oncontextmenu="return false" src="<?php echo htmlspecialchars($_GET['img']); ?>" /&#62;&lt;/a&#62; 
+&lt;a href="http://www.webarchiv.cz/cs/certifikovano" onclick="return !window.open(this, 'kod', 'toolbar=no, menubar=no, directories=no, resizable=yes, status=no, width=600, height=210, top=200, left=50')"&#62; &lt;img alt="Stránky jsou archivovány Národní knihovnou ČR" style="border:none" oncontextmenu="return false" src="<?php echo htmlspecialchars($_GET['img']); ?>" /&#62;&lt;/a&#62; 
 </p>
 </body>
 </html>
